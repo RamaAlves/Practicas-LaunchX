956b092316890496f014922578f0acf2f57574db
RamaAlves <ramiroalvesprof@outlook.com>
2022-11-26 02:20:50 -0300

    Reorganizando repositorio

--- practicas-front/react/contextapp/src/App.js
+++ practicas-front/react/contextapp/src/App.js
@@ -11,14 +11,14 @@ function App() {
 
   );
 }
-
+/* 
 function User(){
   const value= React.useContext(UserContext);
   return (<h1>{value}</h1>);
   /* 
   return (<UserContext.Consumer>
     {value=><h1>{value}</h1>}
-  </UserContext.Consumer>) */
-}
+  </UserContext.Consumer>) 
+} */
 
 export default App;
